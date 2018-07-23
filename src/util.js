447ffd9e1f338cd75912d54a39665d2cae1ddf86
Amit Gupta <amitgupta.gwl@gmail.com>
2018-07-23 17:38:29 +0530

    Supports simple use of regular expression

--- src/util.js
+++ src/util.js
@@ -1,14 +1,14 @@
 const RandExp = require('randexp');
 
-RandExp.prototype.randInt = (a,b) => {
+RandExp.prototype.randInt = (a, b) => {
     return a;
 }
 
-exports.doesMatch = function(a, b) {
+exports.doesMatch = function (a, b) {
     var a_IsRegex = a.indexOf("(") > 0;
     var b_IsRegex = b.indexOf("(") > 0;
 
-    if(a_IsRegex && b_IsRegex){
+    if (a_IsRegex && b_IsRegex) {
         //genetate random string for both regex and pass to the other regex
 
         var aRegex = new RegExp(a);
@@ -17,49 +17,54 @@ exports.doesMatch = function(a, b) {
         var aRand = new RandExp(aRegex).gen();
         var bRand = new RandExp(bRegex).gen();
 
-        if(aRand === bRand){
+        if (aRand === bRand) {
             return true;
-        }else{
-            return exports.doesMatch(a,bRand) || exports.doesMatch(aRand,b);
+        } else {
+            return exports.doesMatch(a, bRand) || exports.doesMatch(aRand, b);
         }
 
-    }else if(!a_IsRegex && b_IsRegex){
+    } else if (!a_IsRegex && b_IsRegex) {
         return new RegExp(b).test(a);
-    }else if(a_IsRegex && !b_IsRegex){
+    } else if (a_IsRegex && !b_IsRegex) {
         return new RegExp(a).test(b);
-    }else /* if(a_IsRegex && b_IsRegex) */{
+    } else /* if(a_IsRegex && b_IsRegex) */ {
         return a === b;
     }
 }
 
-exports.getFirstMatche = function(string, regex_str) {
+exports.getFirstMatche = function (string, regex_str) {
     var regex = new RegExp(regex_str);
     return regex.exec(string);
 }
 
-exports.getAllMatches = function(string, regex_str) {
-    var regex = new RegExp(regex_str,"g");
-    return exports.getAllRegexMatches(string,regex);
+exports.getAllMatches = function (string, regex_str) {
+    var regex = new RegExp(regex_str, "g");
+    return exports.getAllRegexMatches(string, regex);
 }
 
-exports.getAllRegexMatches = function(string, regex) {
+exports.getNamedExpressionMatches = function (string, regex_str) {
+    var regex = new RegExp(regex_str, "g");
+    return exports.getAllRegexMatches(string, regex);
+}
+
+exports.getAllRegexMatches = function (string, regex) {
     var matches = [];
     var match;
     while (match = regex.exec(string)) {
         var allmatches = [];
-        for(var i in match){
+        for (var i in match) {
             var submatch = match[i];
             allmatches.push(submatch);
         }
-      matches.push(allmatches);
+        matches.push(allmatches);
     }
     return matches;
 }
 
-exports.urlSlice = function(url){
+exports.urlSlice = function (url) {
     var index = url.indexOf("?");
-    if(index > 0) return url.substr(0,index);
+    if (index > 0) return url.substr(0, index);
     var index = url.indexOf("#");
-    if(index > 0) return url.substr(0,index);
+    if (index > 0) return url.substr(0, index);
     return url;
 }
--- tests/anumargak_test.js
+++ tests/anumargak_test.js
@@ -387,6 +387,78 @@ describe("Anumargak ", function () {
         expect(anumargak.find("DELETE", "/this/is/dynamic")()).toEqual(30);
     });
 
+    it('should register named regular expression', function () {
+        var anumargak = Anumargak();
+
+        anumargak.addNamedExpression("num", "\\d+");
+
+        expect(Object.keys(anumargak.namedExpression).length).toEqual(1);
+
+        anumargak.addNamedExpression({
+            "oneNum": "\\d",
+            "oneOrNoNum": "\\d?",
+        });
+
+        expect(Object.keys(anumargak.namedExpression).length).toEqual(3);
+
+    });
+
+    it("should throw error when addNamedExpression method called with invalid arguments", function () {
+        var anumargak = Anumargak();
+        var errorMsg = "Invalid method argument. Two parameters of type String or only object is expected.";
+
+        expect(() => {
+            anumargak.addNamedExpression("num");
+        }).toThrowError(errorMsg);
+
+        expect(() => {
+            anumargak.addNamedExpression(123, 234);
+        }).toThrowError(errorMsg);
+
+        expect(() => {
+            anumargak.addNamedExpression([{ "num": "\\d+" }]);
+        }).toThrowError(errorMsg);
+
+    });
+
+    it("should throw error when named expression being used but not defined", function () {
+        var anumargak = Anumargak();
+
+        expect(() => {
+            anumargak.get("/this/is/:dynamic/with/:pattern(:num:)", () => 30);
+        }).toThrowError("Usage of named expression in url as num. Define it using addNamedExpression method before using in URLs.");
+
+    });
+
+    it("should add named regular expression which can be used in dynamic routes for 'GET, POST, DELETE, PUT, HEAD' methods", function () {
+        var anumargak = Anumargak();
+
+        anumargak.addNamedExpression("num", "\\d+");
+        anumargak.addNamedExpression({
+            "oneNum": "\\d",
+            "oneOrNoNum": "\\d?",
+        });
+
+        anumargak.get("/this/is/:dynamic/with/:pattern(:num:)", () => 30);
+        anumargak.head("/this/is/:dynamic/with/:pattern(:oneNum:)", () => 30);
+        anumargak.post("/this/is/:dynamic/with/:pattern(:oneOrNoNum:)", () => 30);
+        anumargak.put("/this/is/:dynamic/with/:pattern(:num:)", () => 30);
+        anumargak.delete("/this/is/:dynamic/with/:pattern(:oneNum:)", () => 30);
+
+        expect(Object.keys(anumargak.dynamicRoutes.GET).length).toEqual(1);
+        expect(Object.keys(anumargak.dynamicRoutes.HEAD).length).toEqual(1);
+        expect(Object.keys(anumargak.dynamicRoutes.POST).length).toEqual(1);
+        expect(Object.keys(anumargak.dynamicRoutes.PUT).length).toEqual(1);
+        expect(Object.keys(anumargak.dynamicRoutes.DELETE).length).toEqual(1);
+        expect(anumargak.count).toEqual(5);
+
+        expect(anumargak.find("GET", "/this/is/dynamic/with/123")()).toEqual(30);
+        expect(anumargak.find("HEAD", "/this/is/dynamic/with/5")()).toEqual(30);
+        expect(anumargak.find("POST", "/this/is/dynamic/with/")()).toEqual(30);
+        expect(anumargak.find("PUT", "/this/is/dynamic/with/41234")()).toEqual(30);
+        expect(anumargak.find("DELETE", "/this/is/dynamic/with/5")()).toEqual(30);
+    });
+
     /* const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
     it("should lookup for aync callback", function(done) {
