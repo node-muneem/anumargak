ef6033d61d7e56de2bdf1ee38cbdceb09897e0fd
Amit Gupta <amitgupta.gwl@gmail.com>
2018-09-21 11:41:34 +0530

    refactoring

--- src/util.js
+++ src/util.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var RandExp = require('randexp');
 
--- tasks/postinstall.js
+++ tasks/postinstall.js
@@ -1,12 +1,10 @@
-//Display message after instalation
+'use strict';
 
-const stars = '\x1B[1m***\x1B[0m';
-const yellow = "\033[1;33m";
-const light_green = "\033[1;32m";
-const light_blue = "\033[1;34m";
-const NC="\033[0m"; // No Color
-
-console.log(yellow+"              Donating to an open source project is more affordable than paying salary"+NC);
+var stars = '\x1B[1m***\x1B[0m';
+var yellow = "\033[1;33m";
+var light_green = "\033[1;32m";
+var light_blue = "\033[1;34m";
+var NC="\033[0m"; // No Color
 
 console.log('');
 console.log('                         ' + stars + ' Thank you for using Anumargak (अनुमार्गक)! ' + stars);
@@ -19,11 +17,4 @@ console.log( light_blue + '          https://www.paypal.com/cgi-bin/webscr?cmd=_
 console.log('');
 console.log('                                            ' + stars);
 
-console.log('');
-console.log('');
-
-console.log('Try' + light_green +' Nimn (निम्न)' + NC + ' format. It can save bandwidth and reduce response size.');
-console.log('');
-console.log('Give us a \x1B[1m*\x1B[0m on '+ light_blue + 'https://github.com/node-muneem/anumargak'+ NC +' and help us to grow');
-console.log('');
 process.exit(0);
