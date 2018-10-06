9e9e8aa36090eaddec7fe27096fa78ff720e2f39
Vera Schöniger <vera.schoeniger@gmail.com>
2018-10-06 17:14:03 +0200

    Added tests

--- tests/anumargak_test.js
+++ tests/anumargak_test.js
@@ -16,6 +16,21 @@ describe("Anumargak ", function () {
         expect(anumargak.quickFind("HEAD", "/this/is/static").handler()).toEqual(30);
     });
 
+    it("should register for multiple urls", function () {
+        var anumargak = Anumargak();
+
+        anumargak.on(["GET", "HEAD"], ["/500", "/502"], () => 30);
+
+        expect(Object.keys(anumargak.staticRoutes.GET).length).toEqual(2);
+        expect(Object.keys(anumargak.staticRoutes.HEAD).length).toEqual(2);
+        expect(anumargak.count).toEqual(4);
+
+        expect(anumargak.quickFind("GET", "/500").handler()).toEqual(30);
+        expect(anumargak.quickFind("HEAD", "/500").handler()).toEqual(30);
+        expect(anumargak.quickFind("GET", "/502").handler()).toEqual(30);
+        expect(anumargak.quickFind("HEAD", "/502").handler()).toEqual(30);
+    });
+
     it("should register for multiple methods", function () {
         var anumargak = Anumargak();
 
